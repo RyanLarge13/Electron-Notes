bdaf11b1a0ed0df2681189544316e127c2d1d35e
RyanLarge13 <ryanlarge13@gmail.com>
2023-12-27 01:00:08 -0500

    Testing

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,7 +14,8 @@ function createWindow(): void {
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
       sandbox: false
-    }
+    },
+    icon: join(__dirname, "../../public.logo.png")
   });
 
   mainWindow.on("ready-to-show", () => {
