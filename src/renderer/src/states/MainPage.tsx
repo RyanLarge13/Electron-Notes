3709ab18e2b009c976a3fb741084545caf4a6374
RyanLarge13 <ryanlarge13@gmail.com>
2024-01-02 22:18:25 -0500

    Creating Key Bindings
    New Feature:
    
    Creating a new feature for a great user experience that will map key combinations within the application to allow quick access and functionality
    to certain operations

--- src/renderer/src/states/MainPage.tsx
+++ src/renderer/src/states/MainPage.tsx
@@ -1,13 +1,81 @@
-import UserContext from "@renderer/contexxt/UserContext";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ClipLoader } from "react-spinners";
+import { useNavigate } from "react-router-dom";
 import Login from "./Login";
 import Account from "./Account";
 import ContextMenu from "@renderer/components/ContextMenu";
 import SystemNotif from "@renderer/components/SystemNotif";
+import UserContext from "@renderer/contexxt/UserContext";
 
 const MainPage = (): JSX.Element => {
-  const { user, loading } = useContext(UserContext);
+  const { user, loading, setOrder, setEdit, setMenu } = useContext(UserContext);
+
+  const [keyPresses, setKeyPressed] = useState({
+    n: false,
+    f: false,
+    Control: false,
+    Alt: false,
+    m: false,
+    e: false,
+    o: false
+  });
+
+  const navigate = useNavigate();
+
+  const handleKeyPress = (e): void => {
+    e.preventDefault();
+    const key = e.key;
+    if (keyPresses.hasOwnProperty(key)) {
+      setKeyPressed((prevPresses) => {
+        return {
+          ...prevPresses,
+          [key]: true
+        };
+      });
+    }
+  };
+
+  const handleKeyUp = (e): void => {
+    e.preventDefault();
+    const key = e.key;
+    if (keyPresses.hasOwnProperty(key)) {
+      setKeyPressed((prevPresses) => {
+        return {
+          ...prevPresses,
+          [key]: false
+        };
+      });
+    }
+  };
+
+  useEffect(() => {
+    const { Alt, Control, f, n, e, o, m } = keyPresses;
+
+    if (Alt && Control && n) {
+      navigate("/newnote");
+    }
+    if (Alt && Control && f) {
+      navigate("/newfolder");
+    }
+    if (Control && e) {
+      setEdit((prev) => !prev);
+    }
+    if (Control && o) {
+      setOrder((prev) => !prev);
+    }
+    if (Control && m) {
+      setMenu((prev) => !prev);
+    }
+  }, [keyPresses]);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, []);
 
   return (
     <section>
